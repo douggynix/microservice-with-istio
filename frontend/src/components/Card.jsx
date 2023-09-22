38e0f78ddcc55dc23f41c8fb5a46dabdede63e1d
James Douglass <douggynix@gmail.com>
2023-09-22 00:25:05 -0400

    Adding destination rules for frontend and Fixing frontend card ui info display

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,13 +1,15 @@
 
 const Card = ({id, title, description}) => {
     console.log('title',title,'description',description)
-    const className=`block w-full max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`
     return(
         // eslint-disable-next-line
-        <a href="#" className={className}>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
-        </a>
+            <div className="card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+                <div className="m-3">
+                    <h2 className="text-lg mb-2">{title}
+                        <span className="text-sm text-teal-800 font-mono bg-teal-100 inline rounded-full px-2 align-top float-right animate-pulse">{id}</span></h2>
+                    <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200 break-words">{description}</p>
+                </div>
+            </div>
     )
 }
 
--- frontend/src/components/CardList.jsx
+++ frontend/src/components/CardList.jsx
@@ -6,11 +6,13 @@ const CardList = ({features}) => {
 
     console.log("Rerender CardList : ",features)
   return (
-    <div className="container mx-auto">
+    <div className="container mt-4 mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {features.map(feature => {
           console.log('from Map',feature)
           return <Card key={feature.id} {...feature} />
         })}
+      </div>
     </div>
   );
     
