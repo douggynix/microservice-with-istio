35ab24bff6ff3c0607a17a9e68c5aee99dffd812
James Douglass <douggynix@gmail.com>
2023-09-22 18:13:57 -0400

    using concurrently utility package to start 2 concurrent backend processes  in dev mode

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,6 @@
 const isDev = process.argv.length > 2 && process.argv[2]==='--dev'
 const BACKEND_NAME = process.env.BACKEND_NAME || 'Backend'
 
-if(isDev){
-    const app1 = require('./app')
-    const port1=3001
-
-    const app2 = require('./app')
-    const port2 = 3002
-
-    console.log('Dev mode Enabled. Starting 2 Backend services')
-    app1.listen(port1,() => {
-        console.log(`${BACKEND_NAME}_1 process is listening on port ${port1}`)
-    })
-
-    app2.listen(port2,() => {
-        console.log(`${BACKEND_NAME}_2 process is listening on port ${port2}`)
-    })
-    return 
-} 
-
 const app = require('./app')
 const PORT = process.env.BACKEND_PORT || 3001
 
